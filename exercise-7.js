6799fb8be675a85f50cbd50a593e7addb5337467
sukmabrahmantya <brahmantyasukma@gmail.com>
2019-09-04 14:14:54 +0700

    update exercise 7 dan 8

--- exercise-7.js
+++ exercise-7.js
@@ -1,7 +1,22 @@
+// function hitungJumlahKata(kalimat) {
+//     var pisah = kalimat.split(" ")
+//     return pisah.length
+//   }
+
 function hitungJumlahKata(kalimat) {
-    var pisah = kalimat.split(" ")
-    return pisah.length
-  }
+  var result = []
+  var temp = ''
+
+  for (var i = 0; i <= kalimat.length; i++){
+    if (kalimat[i] === ' ' || i == kalimat.length) {
+        result.push(temp)
+        temp = ''
+      } else {
+        temp += kalimat[i]
+      }
+    }
+    return result.length
+}
   
   // TEST CASES
   console.log(hitungJumlahKata('I have a dream')); // 4
--- exercise-8.js
+++ exercise-8.js
@@ -1,11 +1,23 @@
 function pasanganTerbesar(number) {
+  var result = []
+  var temp = ''
+  var numString = String(number)
 
-
+  for (var i = 0; i <= numString.length; i++){
+    if (temp.length === 2){
+      result.push(Number(temp))
+      temp = ''
+      i -= 2
+    } else {
+      temp = temp + numString[i]
+    }
   }
-  
+  result.sort(function(a,b){return b-a});
+  return result[0];
+}  
 
 
-  TEST CASES
+  //TEST CASES
   console.log(pasanganTerbesar(641573)); // 73
   console.log(pasanganTerbesar(12783456)); // 83
   console.log(pasanganTerbesar(910233)); // 91
